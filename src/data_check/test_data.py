6380af0e877a68d3638c81a561611c733d0dcd72
Ross <rf@rosss-mbp.home>
2022-08-12 19:24:45 +0100

    updated data_check  component

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -64,4 +64,15 @@ def test_row_count(data):
     assert 15000 < data.shape[0] < 1000000
 
 def test_price_range(data, min_price, max_price):
-    assert data['price'].between(float(min_price), float(max_price))
+    assert data['price'].between(min_price, max_price)
+    # assert data['price'].between(float(min_price), float(max_price))
+
+
+
+
+
+
+
+
+
+
