66712c311fd6e3626034451dbc6bb2b272cc02b9
Ross <rf@rosss-mbp.home>
2022-08-12 20:08:59 +0100

    updated data_check  component

--- src/data_check/test_data.py
+++ src/data_check/test_data.py
@@ -63,8 +63,12 @@ def test_similar_neigh_distrib(data: pd.DataFrame, ref_data: pd.DataFrame, kl_th
 def test_row_count(data):
     assert 15000 < data.shape[0] < 1000000
 
-def test_price_range(data: pd.DataFrame, min_price, max_price):
-    assert data['price'].between(min_price, max_price)
+# def test_price_range(data: pd.DataFrame, min_price, max_price):
+    """
+    Test price between min and max is same as price
+    """
+def test_price_range(data, min_price, max_price):
+    assert data['price']=data['price'].between(min_price, max_price)
     # assert data['price'].between(float(min_price), float(max_price))
 
 
