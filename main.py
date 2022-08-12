0fb660875a2e62778873a45c4a4aa47a1f74c3ad
Ross <rf@rosss-mbp.home>
2022-08-12 19:22:17 +0100

    updated data_check  component

--- main.py
+++ main.py
@@ -75,7 +75,7 @@ def go(config: DictConfig):
                 parameters={
                     "csv": "clean_sample.csv:latest",
                     "ref": "clean_sample.csv:reference",
-                    "kl_threshold": "config['data_check']['kl_threshold']",
+                    "kl_threshold": config["data_check"]["kl_threshold"],
                     "min_price": config['etl']['min_price'],
                     "max_price": config['etl']['max_price']
                 },
