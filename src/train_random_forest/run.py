8956d48edfa3fa5f31c4cd8043ba457dd80ef07d
Ross <rf@rosss-mbp.home>
2022-08-17 23:43:00 +0100

    updated train_random_forest component

--- src/train_random_forest/run.py
+++ src/train_random_forest/run.py
@@ -221,7 +221,7 @@ def get_inference_pipeline(rf_config, max_tfidf_features):
     sk_pipe = Pipeline(
         steps=[
             ("preprocessor", preprocessor),
-            ("random_forest", random_forest)
+            ("random_forest", random_Forest)
         ]
     )
 
